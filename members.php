2de75a1e78aaa07bbec5311e55332db1ed045113
Karl Stephen Evallo <71174588+MiyokoShinzu@users.noreply.github.com>
2025-09-09 02:33:29 +0800

    Update members.php

--- members.php
+++ members.php
@@ -334,12 +334,12 @@
 
 
         </div>
-        <div class="row mb-3 mt-3   shadow bg-white p-3">
+        <div class="row mb-3 mt-3   shadow bg-white">
             <div class="col-lg-10 col-11 mx-auto mt-3 mb-3">
                 <h5 class="tentative text-center fw-bold border-bottom border-primary border-2 pb-2">Tentative Members of the Club</h5>
 
             </div>
-            <div class="col-lg-10 mb-4 text-center mx-auto  mt-1">
+            <div class="col-lg-1 mb-4 text-center mx-auto  mt-1">
                 <span class=" mb-2 mt-2 text-danger text-center">
                     Tentative members are required to take an examination before initial orientation. The exam schedule and procedures will be posted later.
 
