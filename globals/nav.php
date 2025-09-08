69d74a997dc4fccd554af3bc4460264c5463d4b2
Karl Stephen Evallo <71174588+MiyokoShinzu@users.noreply.github.com>
2025-09-09 02:01:20 +0800

    Update nav.php

--- globals/nav.php
+++ globals/nav.php
@@ -12,9 +12,7 @@
                 <li class="nav-item">
                     <a href="index.php" class="nav-link">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a href="#about" class="nav-link">About</a>
-                </li>
+               
                 <li class="nav-item">
                     <a href="" class="nav-link">Services</a>
                 </li>
